06e0d0a41df3c7b974b2a6d212c3fdd687d1c71c
Jakub Skokan <jakub.skokan@havefun.cz>
2023-03-19 09:18:15 +0100

    Armament: add support for burst fire

--- armament.lua
+++ armament.lua
@@ -8,7 +8,12 @@ function Armament:add(name, opts)
   self.weapons[name] = {
     frequency = opts.frequency,
     fire = opts.fire,
-    lastshot = nil,
+    lastShot = nil,
+    burstFrequency = opts.burstFrequency or nil,
+    burstShots = opts.burstShots or 3,
+    burstOpen = false,
+    currentBurstShots = 0,
+    lastBurst = nil,
   }
 end
 
@@ -35,10 +40,43 @@ function Armament:fireAll(time)
 end
 
 function Armament:fireWeapon(weapon, time)
-  if weapon.lastshot == nil or weapon.lastshot + weapon.frequency < time then
-    weapon.fire()
-    weapon.lastshot = time
+  if weapon.burstFrequency then
+    self:fireBurst(weapon, time)
+  else
+    self:fireShot(weapon, time)
   end
+end
+
+function Armament:fireBurst(weapon, time)
+  if weapon.burstOpen then
+    self:fireShotInBurst(weapon, time)
+  elseif not weapon.lastBurst or weapon.lastBurst + weapon.burstFrequency < time then
+    weapon.burstOpen = true
+    weapon.lastShot = nil
+    self:fireShotInBurst(weapon, time)
+  end
+end
+
+function Armament:fireShotInBurst(weapon, time)
+  if self:fireShot(weapon, time) then
+    weapon.currentBurstShots = weapon.currentBurstShots + 1
+
+    if weapon.currentBurstShots >= weapon.burstShots then
+      weapon.burstOpen = false
+      weapon.currentBurstShots = 0
+      weapon.lastBurst = time
+    end
+  end
+end
+
+function Armament:fireShot(weapon, time)
+  if weapon.lastShot == nil or weapon.lastShot + weapon.frequency < time then
+    weapon.fire()
+    weapon.lastShot = time
+    return true
+  end
+
+  return false
 end
 
 return Armament
